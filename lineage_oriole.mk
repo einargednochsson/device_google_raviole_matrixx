e3b06532aba7d57a3770014597e1050604a08b41
Einar Gednochsson <pete80pro.pb@gmail.com>
2024-06-02 13:51:13 +0200

    raviole: Matrixxi'fy

--- lineage_oriole.mk
+++ lineage_oriole.mk
@@ -8,6 +8,29 @@
 TARGET_DISABLE_EPPE := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Matrixx
+MATRIXX_BUILD_TYPE := Official
+MATRIXX_MAINTAINER := EinarG.
+MATRIXX_CHIPSET := GS101
+MATRIXX_BATTERY := 4614mAh
+MATRIXX_DISPLAY := 1080x2400
+
+# Matrixx Flags
+WITH_GMS := true
+TARGET_HAS_UDFPS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_INCLUDE_PIXEL_FRAMEWORK := true
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_PIXEL_BOOT_ANIMATION_RES := 1080
+TARGET_IS_PIXEL := true
+TARGET_IS_PIXEL_6 := true
+TARGET_PIXEL_STAND_SUPPORTED := true
+TARGET_EXCLUDES_AUXIO := true
+TARGET_EXCLUDES_VIA := true
+BUILD_GOOGLE_CONTACTS := true
+BUILD_GOOGLE_DIALER := true
+BUILD_GOOGLE_MESSAGE := true
+
 # Inherit device configuration
 $(call inherit-product, device/google/raviole/aosp_oriole.mk)
 $(call inherit-product, device/google/gs101/lineage_common.mk)
--- lineage_raven.mk
+++ lineage_raven.mk
@@ -8,6 +8,29 @@
 TARGET_DISABLE_EPPE := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Matrixx
+MATRIXX_BUILD_TYPE := Official
+MATRIXX_MAINTAINER := EinarG.
+MATRIXX_CHIPSET := TensorG1
+MATRIXX_BATTERY := 5000mAh
+MATRIXX_DISPLAY := 1440x3120
+
+# Matrixx Flags
+WITH_GMS := true
+TARGET_HAS_UDFPS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_INCLUDE_PIXEL_FRAMEWORK := true
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_PIXEL_BOOT_ANIMATION_RES := 1440
+TARGET_IS_PIXEL := true
+TARGET_IS_PIXEL_6 := true
+TARGET_PIXEL_STAND_SUPPORTED := true
+TARGET_EXCLUDES_AUXIO := true
+TARGET_EXCLUDES_VIA := true
+BUILD_GOOGLE_CONTACTS := true
+BUILD_GOOGLE_DIALER := true
+BUILD_GOOGLE_MESSAGE := true
+
 # Inherit device configuration
 $(call inherit-product, device/google/raviole/aosp_raven.mk)
 $(call inherit-product, device/google/gs101/lineage_common.mk)
